75dc566367a3c0b0dced1a529fe5f1109d880af9
Javier Molina <javier.molina@unit4.com>
2016-08-26 10:42:43 +0200

    Added test class

---
+++ AnagramSolver.Tests/AnagramHelperTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramSolver.Tests
+{
+    [TestClass]
+    public class AnagramHelperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
